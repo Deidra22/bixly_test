ac23476620e53a2f25f39a1bb27a4857e2dfb533
deidra <deidraann22@gmail.com>
2020-06-20 23:08:45 -0700

    refact(urls): made the api root more efficient and combines urls.py files

--- garage_manager/cars/urls.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import routers
-from garage_manager.api import CarViewSet
-
-router = routers.DefaultRouter()
-router.register('api/cars', CarViewSet, 'cars' )
-
-urlpatterns = router.urls
--- garage_manager/garage_manager/urls.py
+++ garage_manager/garage_manager/urls.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
+from django.conf.urls import url
 from django.urls import path, include
+from rest_framework import routers
+from .api import CarViewSet
+from .api import TruckViewSet
+from .api import BoatViewSet
+
+
+router = routers.DefaultRouter()
+router.register(r'api/cars', CarViewSet)
+router.register(r'api/trucks', TruckViewSet)
+router.register(r'api/boats', BoatViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('cars.urls')),
-    path('', include('trucks.urls')),
-    path('', include('boats.urls')),
+    path('', include(router.urls)),
 ]
